f55efd339894627d38ce2811dfa0044c4a568942
Mahatmaditya FRS <40485390+Mafarasya@users.noreply.github.com>
2021-09-01 10:55:58 +0700

    Add files update for navbar responsive

---
+++ main.js
@@ -0,0 +1,11 @@
+
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("#navbar ul");
+
+menuToggle.addEventListener('click', function(){
+    nav.classList.toggle('slide');
+})
+
+// Navbar scrolling visible
+
+
