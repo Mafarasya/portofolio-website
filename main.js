5f3014e139f4b90952b64b3cbaf4dff462211ea8
Mahatmaditya FRS <40485390+Mafarasya@users.noreply.github.com>
2021-09-03 16:19:27 +0700

    add main.js, darkmode

--- main.js
+++ main.js
@@ -6,6 +6,26 @@ menuToggle.addEventListener('click', function(){
     nav.classList.toggle('slide');
 })
 
-// Navbar scrolling visible
+$(document).ready(function () {
+    $(window).on('load', function(){
+    $('.my-profile').addClass('appear');
+    $('.welcome-title').addClass('appear');
+})
+  });
+
+  const toggleSwitch = document.querySelector('.theme-switch-wrapper');
+  function switchTheme(e) {
+      if(e.target.checked) {
+          document.documentElement.setAttribute('data-theme', 'dark');
+          localStorage.setItem('theme', 'dark');
+      } 
+      else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+      }
+  }
+
+  toggleSwitch.addEventListener('change', switchTheme, false);
+
 
 
